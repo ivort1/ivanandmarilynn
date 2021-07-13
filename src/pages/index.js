7f5fe5940111ac991a9967d0b92c1d1b6d6bfcc5
ivunortiz <ivan.ortiz1@ingka.ikea.com>
2021-07-13 15:40:20 -0400

    Adjusted the borders again

--- src/pages/index.js
+++ src/pages/index.js
@@ -188,11 +188,11 @@ const IndexPage = () => {
 
         {/* FAQs */}
         <div className="faq-section" id="faq-section">
-          <div className="custom-shape-divider-top-1625982302">
+          {/* <div className="custom-shape-divider-top-1625982302">
             <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
               <path d="M1200 120L0 16.48 0 0 1200 0 1200 120z" className="shape-fill"></path>
             </svg>
-          </div>
+          </div> */}
           
           <div className="header">{t('nav-faqs')}</div>
 
